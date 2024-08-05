f3958f6957f11aa1af966e0bd859cd324d910741
Sergei Zemnitsky <zema.working@yandex.by>
2024-08-05 11:11:01 +0300

    feat: new index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let config = bracketsConfig.map ( (e) => e.join('') );
+
+  return strProcessing(str, config) ? false : true;
+}
+
+function strProcessing (str, arg) {
+  for (let i = 0; i < arg.length; i++) {
+    if (str.includes(arg[i])) {
+      str = str.replace(arg[i], '');
+      i = -1;
+    }
+  }
+  return str;
 }
